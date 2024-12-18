770990f55bf0fdef6d8e4a39f7f5c0051faef378
imnaK <mc.yama1000@gmail.com>
2024-12-18 17:41:58 +0100

    feat: solve day-2 part-1

--- day-2/part-1.go
+++ day-2/part-1.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -14,14 +15,59 @@ func main() {
 	}
 
 	content := strings.Split(string(raw), "\n")
+	passed := 0
 
 	for _, line := range content {
 		if line == "" {
 			continue
 		}
 
-		fmt.Println(line)
+		// get levels as array
+		levels := strings.Split(line, " ")
+
+		// check for ascending
+		firstNum, err := strconv.Atoi(levels[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		secondNum, err := strconv.Atoi(levels[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		ascending := firstNum < secondNum
+		passing := true
+
+		// check all levels
+		for i := 1; i < len(levels); i++ {
+			lastNum, err := strconv.Atoi(levels[i-1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			currNum, err := strconv.Atoi(levels[i])
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// check if still ascending/decending
+			if (lastNum < currNum) != ascending {
+				passing = false
+				break
+			}
+
+			// check if number range is too big
+			diff := getDiff(lastNum, currNum)
+			if diff < 1 || diff > 3 {
+				passing = false
+				break
+			}
+		}
+
+		if passing {
+			passed++
+		}
 	}
+
+	fmt.Println(passed)
 }
 
 func getDiff(num1 int, num2 int) int {
