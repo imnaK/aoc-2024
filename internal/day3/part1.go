f617298e8384ccb7ca3056e3869eb0014b99fb00
imnaK <mc.yama1000@gmail.com>
2024-12-23 00:49:52 +0100

    feat: solve day-3 part-1

--- internal/day3/part1.go
+++ internal/day3/part1.go
@@ -1,5 +1,40 @@
 package day3
 
-func Day3Part1(data string) {
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
+func Day3Part1(data string) {
+	validMuls := regexp.MustCompile(`mul\(\d+,\d+\)`)
+	sum := 0
+
+	matches := validMuls.FindAllString(data, -1)
+	for _, match := range matches {
+		parsedStmt, err := parseStatement(match)
+		if err != nil {
+			fmt.Println(fmt.Errorf("Failed to parse statement: %w", err))
+		}
+
+		sum += parsedStmt
+	}
+
+	fmt.Println(sum)
+}
+
+func parseStatement(stmt string) (int, error) {
+	stmtSplit := strings.Split(stmt, ",")
+	firstNum, err := strconv.Atoi(stmtSplit[0][4:])
+	if err != nil {
+		return 0, errors.New("First parameter is not a whole number")
+	}
+	secondNum, err := strconv.Atoi(stmtSplit[1][:len(stmtSplit[1])-1])
+	if err != nil {
+		return 0, errors.New("Second parameter is not a whole number")
+	}
+
+	return firstNum * secondNum, nil
 }
