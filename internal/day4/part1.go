3d93df1c440d1c40205bd0e2b6db47eb732ac550
imnaK <mc.yama1000@gmail.com>
2024-12-25 04:22:36 +0100

    feat: make day-4 part-1 more efficient

--- internal/day4/part1.go
+++ internal/day4/part1.go
@@ -10,18 +10,30 @@ type Direction int
 
 const (
 	horizontal Direction = iota
-	horizontalReversed
 	vertical
-	verticalReversed
 	diagonalTopLeft
 	diagonalTopRight
-	diagonalTopLeftReversed
-	diagonalTopRightReversed
 )
 
-const xmas string = "XMAS"
+const searchWord string = "XMAS"
+const searchWordReversed string = "SAMX"
 
 func Day4Part1(data string) any {
+	//startOwn := time.Now()
+	//for i := 0; i < 100; i++ {
+	//	solve(data)
+	//}
+	//durationOwn := time.Since(startOwn)
+
+	//startOther := time.Now()
+	//for i := 0; i < 100; i++ {
+	//	countWordOccurrences(data, searchWord)
+	//}
+	//durationOther := time.Since(startOther)
+
+	//resOwn := solve(data)
+	//return fmt.Sprintf("Own: %v | Other: %v\nResOwn: %v", durationOwn, durationOther, resOwn)
+
 	return solve(data)
 }
 
@@ -32,35 +44,31 @@ func solve(data string) any {
 	dataAsBytes := []byte(strings.ReplaceAll(data, "\n", ""))
 	founds := 0
 
-	for _, direction := range []Direction{horizontal, horizontalReversed, vertical, verticalReversed, diagonalTopLeft, diagonalTopLeftReversed, diagonalTopRight, diagonalTopRightReversed} {
-		rotatedPuzzle, searchString := getPuzzleRotated(dataAsBytes, direction, puzzleWidth, puzzleHeight)
-
-		for _, line := range rotatedPuzzle {
-			founds += strings.Count(line, searchString)
-		}
-	}
+	founds += countForwardAndBackward(getPuzzleRotated(dataAsBytes, horizontal, puzzleWidth, puzzleHeight, searchWord))
+	founds += countForwardAndBackward(getPuzzleRotated(dataAsBytes, vertical, puzzleWidth, puzzleHeight, searchWord))
+	founds += countForwardAndBackward(getPuzzleRotated(dataAsBytes, diagonalTopLeft, puzzleWidth, puzzleHeight, searchWord))
+	founds += countForwardAndBackward(getPuzzleRotated(dataAsBytes, diagonalTopRight, puzzleWidth, puzzleHeight, searchWord))
 
 	return founds
 }
 
-func getPuzzleRotated(data []byte, direction Direction, puzzleWidth int, puzzleHeight int) ([]string, string) {
-	searchString := []byte(xmas)
+func countForwardAndBackward(puzzle []string) int {
+	joinedPuzzle := strings.Join(puzzle, "\n")
+	return strings.Count(joinedPuzzle, searchWord) + strings.Count(joinedPuzzle, searchWordReversed)
+}
+
+func getPuzzleRotated(data []byte, direction Direction, puzzleWidth int, puzzleHeight int, searchWord string) []string {
 	var rotatedData []string
 
 	switch direction {
-	case horizontalReversed, verticalReversed, diagonalTopLeftReversed, diagonalTopRightReversed:
-		utils.ReverseArray(searchString)
-	}
-
-	switch direction {
-	case horizontal, horizontalReversed:
+	case horizontal:
 		rotatedData = make([]string, puzzleHeight)
 		for y := range puzzleHeight {
 			offset := y * puzzleWidth
 			rotatedData[y] = string(data[offset : offset+puzzleWidth])
 		}
 		break
-	case vertical, verticalReversed:
+	case vertical:
 		rotatedData = make([]string, puzzleHeight)
 		for y := range puzzleHeight {
 			newLine := make([]byte, puzzleHeight)
@@ -71,8 +79,8 @@ func getPuzzleRotated(data []byte, direction Direction, puzzleWidth int, puzzleH
 			rotatedData[y] = string(newLine)
 		}
 		break
-	case diagonalTopLeft, diagonalTopLeftReversed:
-		offset := len(searchString) - 1
+	case diagonalTopLeft, diagonalTopRight:
+		offset := len(searchWord) - 1
 		rotatedData = make([]string, puzzleHeight+puzzleWidth-1-offset*2)
 		for y := offset; y < puzzleHeight; y++ {
 			newLineLen := y + 1
@@ -84,7 +92,12 @@ func getPuzzleRotated(data []byte, direction Direction, puzzleWidth int, puzzleH
 
 			for z := range newLineLen {
 				someY := puzzleHeight - y - 1 + z
-				dataIdx := someY*puzzleWidth + z
+				var dataIdx int
+				if direction == diagonalTopLeft {
+					dataIdx = someY*puzzleWidth + z
+				} else {
+					dataIdx = someY*puzzleWidth + (puzzleWidth - z - 1)
+				}
 				newLine[z] = data[dataIdx]
 			}
 
@@ -101,45 +114,12 @@ func getPuzzleRotated(data []byte, direction Direction, puzzleWidth int, puzzleH
 			newLine := make([]byte, newLineLen)
 
 			for z := range newLineLen {
-				someX := puzzleWidth - x - 1 + z
-				dataIdx := z*puzzleWidth + someX
-				newLine[z] = data[dataIdx]
-			}
-
-			rotatedData[x+puzzleHeight-offset*2] = string(newLine)
-		}
-		break
-	case diagonalTopRight, diagonalTopRightReversed:
-		offset := len(searchString) - 1
-		rotatedData = make([]string, puzzleHeight+puzzleWidth-1-offset*2)
-		for y := offset; y < puzzleHeight; y++ {
-			newLineLen := y + 1
-			if newLineLen > puzzleWidth-1 {
-				newLineLen = puzzleWidth
-			}
-
-			newLine := make([]byte, newLineLen)
-
-			for z := range newLineLen {
-				someY := puzzleHeight - y - 1 + z
-				dataIdx := someY*puzzleWidth + (puzzleWidth - z - 1)
-				newLine[z] = data[dataIdx]
-			}
-
-			rotatedData[y-offset] = string(newLine)
-		}
-
-		// minus 1 for the mid diagonal row which already was added by above
-		for x := offset; x < puzzleWidth-1; x++ {
-			newLineLen := x + 1
-			if newLineLen > puzzleHeight-1 {
-				newLineLen = puzzleHeight
-			}
-
-			newLine := make([]byte, newLineLen)
-
-			for z := range newLineLen {
-				someX := x - z
+				var someX int
+				if direction == diagonalTopLeft {
+					someX = puzzleWidth - x - 1 + z
+				} else {
+					someX = x - z
+				}
 				dataIdx := z*puzzleWidth + someX
 				newLine[z] = data[dataIdx]
 			}
@@ -149,7 +129,44 @@ func getPuzzleRotated(data []byte, direction Direction, puzzleWidth int, puzzleH
 		break
 	}
 
-	return rotatedData, string(searchString)
+	return rotatedData
+}
+
+func countWordOccurrences(data string, word string) int {
+	board := utils.RemoveEmpty(strings.Split(data, "\n"))
+
+	rows, cols := len(board), len(board[0])
+	count := 0
+	directions := [][]int{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+
+	var search func(i, j, dir int, index int) bool
+	search = func(i, j, dir int, index int) bool {
+		if index == len(word) {
+			return true
+		}
+		if i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] != word[index] {
+			return false
+		}
+		return search(i+directions[dir][0], j+directions[dir][1], dir, index+1)
+	}
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if board[i][j] == word[0] {
+				for dir := 0; dir < 8; dir++ {
+					if search(i, j, dir, 0) {
+						count++
+					}
+				}
+			}
+		}
+	}
+
+	return count
 }
 
 func getFoundWords(data string, words []string) any {
