3d9f5cd80a9148391d95a1d719aef5e9e563b16f
imnaK <mc.yama1000@gmail.com>
2024-12-26 20:08:54 +0100

    feat: solve day-4 part-2

--- internal/day4/part2.go
+++ internal/day4/part2.go
@@ -1,5 +1,37 @@
 package day4
 
+import (
+	"strings"
+)
+
 func Day4Part2(data string) any {
-	return nil
+	height := strings.Count(data, "\n")
+	width := strings.Index(data, "\n")
+
+	dataAsBytes := []byte(strings.ReplaceAll(data, "\n", ""))
+	founds := 0
+
+	for y := 1; y < height-1; y++ {
+		for x := 1; x < width-1; x++ {
+			pos := y*width + x
+
+			if dataAsBytes[pos] == 'A' {
+				tl := pos - width - 1
+				br := pos + width + 1
+				left := (dataAsBytes[tl] + dataAsBytes[br]) == 160
+
+				if left {
+					tr := pos - width + 1
+					bl := pos + width - 1
+					right := (dataAsBytes[tr] + dataAsBytes[bl]) == 160
+
+					if right {
+						founds++
+					}
+				}
+			}
+		}
+	}
+
+	return founds
 }
